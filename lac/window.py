ad08e9939efe41d7d23ac654364a1be92e72996c
kamronb <kamron@cmit.com.jm>
2019-10-22 20:42:15 -0500

    Created Dummy menus

--- lac/window.py
+++ lac/window.py
@@ -1,8 +1,25 @@
 from tkinter import *
+
 game_window = Tk()
 
+#setting the menu for the game
+game_menu = Menu(game_window)
+
+
+# Name of the video game in the title bar
 game_window.title("Video Game Name Here")
 
-game_window.configure(width=500, height=500)
+file_menu = Menu(game_menu, tearoff = 0)
+file_menu.add_command(label="New", command = donothing)
+
+
+
+
+
+
+
+
+
+game_window.configure(width=1000, height=750)
 
 game_window.mainloop()
