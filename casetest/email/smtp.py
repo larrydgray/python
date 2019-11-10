61c05934e5840a186345473acf27952fc37c5c4b
Larry Gray <javacaverdude@yahoo.com>
2019-11-10 12:42:12 -0600

    send text attachment and zip attachments

--- casetest/email/smtp.py
+++ casetest/email/smtp.py
@@ -2,8 +2,9 @@ import smtplib
 smtpObj = smtplib.SMTP('smtp.mail.yahoo.com',587)
 smtpObj.ehlo()
 smtpObj.starttls()
-smtpObj.login('javacaverdude@yahoo.com','abplhoqkwzjrmype')
+smtpObj.login('javacaverdude@yahoo.com','xxxxxxxx')
 smtpObj.sendmail('javacaverdude@yahoo.com',
                  'javacaverdude@gmail.com', 
                  'Subject: Test\n\n Testing email system.' )
+
 smtpObj.quit()
