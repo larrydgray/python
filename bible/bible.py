decd6ad5b48bb03da93ba866d6cd712a261762ce
Larry Gray <javacaverdude@yahoo.com>
2019-11-11 23:06:16 -0600

    Works with the 4 Gospels

--- bible/bible.py
+++ bible/bible.py
@@ -7,14 +7,15 @@ def load_book(name):
 
     i=0
     while(i<len(book)):
-        verse=''
+        verse_num=''
         verse_text=''
         #print(c,end='')
         if(book[i]=='{'):
             while(book[i]!='}'):
-                verse+=book[i]
+                verse_num+=book[i]
                 i+=1
-            verse+='}'
+            verse_num+='}'
+            
             i+=1
             while(book[i]!='{'):
                 verse_text+=book[i]
@@ -22,20 +23,61 @@ def load_book(name):
                 if(i==len(book)):
                     break
             i-=1
+            chapter,verse = split_verse(verse_num)
+            the_verse=Verse(name,chapter,verse, verse_text)
+            verses[the_verse.getId()]=the_verse
         i+=1
-        verses[verse]=verse_text
-    return verses
 
+    return verses
+        
+def split_verse(verse_text):
+        verse_split=verse_text.split(':')
+        chapter=verse_split[0][1:]
+        verse=verse_split[1][:1]
+        return (chapter, verse)
+
+def build_verse_id(book, verse_input):
+        chapter, verse = split_verse(verse_input)
+        return book+':'+chapter+':'+verse
+
+class Verse:
+
+    def __init__(self, book, chapter, verse, verse_text):
+        self.book = book
+        self.chapter = chapter
+        self.verse = verse
+        self.verse_text = verse_text.strip()
+
+    def getId(self):
+        return self.book+':'+self.chapter+':'+self.verse
+    def get_book(self):
+        return self.book
+    def get_chapter(self):
+        return self.chapter
+    def get_verse(self):
+        return self.verse
+    def get_verse_text(self):
+        return self.verse_text
 
 book_verses=load_book('Matthew')
+book_verses.update(load_book('Mark'))
+book_verses.update(load_book('Luke'))
+book_verses.update(load_book('John'))
+
 selected_verse=''
 while(selected_verse!='stop'):
-    print('Enter {c:v} or stop')
+    print('Enter b:c:v or stop')
     selected_verse=input()
     if(selected_verse=='stop'):
         break
-    #print(selected_verse)
-    print(book_verses[selected_verse])
+    try:
+        a_verse = book_verses[selected_verse]
+        print()
+        print(a_verse.get_verse_text())
+        print()
+    except KeyError:
+        print('Verse Not Found')
+    
 
 
 
---
+++ bible/verse_split.py
@@ -0,0 +1,4 @@
+verse='{1:2}'
+verse_split=verse.split(':')
+print('Chapter '+verse_split[0][1:])
+print('Verse '+verse_split[1][:1])
