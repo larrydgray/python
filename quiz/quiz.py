066d07c31f1adaca7a613291a11479561903e5d3
Larry Gray <javacaverdude@yahoo.com>
2019-11-26 13:20:13 -0600

    fixes

--- quiz/quiz.py
+++ quiz/quiz.py
@@ -7,12 +7,14 @@ def processcard(card, cycle_idlist):
             ch = msvcrt.getch()
             if ch == b'l':  
                 print('Leaving')
+                leitner.removeidfromtestcycle(card.catagory_id,'quizs\\', testcyclefilename)
                 cycle_idlist.remove(card.catagory_id)
                 break  
             elif ch == b'p':
                 print('Promoting')
                 leitner.moveidtobox(card.catagory_id,'quizs\\', card.box+1, card.box)
                 card.box+=1
+                leitner.removeidfromtestcycle(card.catagory_id,'quizs\\', testcyclefilename)
                 cycle_idlist.remove(card.catagory_id)
                 break
             elif ch == b'd':
@@ -20,6 +22,7 @@ def processcard(card, cycle_idlist):
                 if card.box>1:
                     leitner.moveidtobox(card.catagory_id,'quizs\\', card.box-1, card.box)
                     card.box-=1
+                    leitner.removeidfromtestcycle(card.catagory_id,'quizs\\', testcyclefilename)
                     cycle_idlist.remove(card.catagory_id)
                 break
             else:
@@ -28,9 +31,15 @@ def processcard(card, cycle_idlist):
 
 cards = leitner.loadcards('quizs\\','cards')
 cycle = leitner.loadtestcycle('quizs\\', cards)
+testcyclefilename='testcycle'+str(cycle.cyclenum)+'.txt'
 print("Cycle number "+str(cycle.cyclenum))
 while True:
     print("Number of questions left in cycle is "+str(len(cycle.idlist)))
+    if  cycle.idlist[0]=='':
+        cycle.idlist[0]='empty'
+        leitner.savetestcyclefile(cycle,'quizs\\', cards)
+        print('End of test cycle.')
+        sys.exit()
     card=cards[cycle.idlist[0]]
     print('ID:'+card.catagory_id+' Box '+str(card.box))
     print('QUESTION: '+card.question)
@@ -45,13 +54,10 @@ while True:
             ch = msvcrt.getch()
             if ch == b'n':  
                 break 
-            elif ch == b'q':
-                try:
-                    sys.exit()      
-                except SystemExit:
-                    print ('Quiting Cycle number '+str(cycle.cyclenum)+' with '+
-                        str(len(cycle.idlist))+' questions left.')
-                    break
+            elif ch == b'q': 
+                print ('Quiting Cycle number '+str(cycle.cyclenum)+' with '+
+                    str(len(cycle.idlist))+' questions left.')
+                sys.exit()      
             else:
                 continue 
     print()
